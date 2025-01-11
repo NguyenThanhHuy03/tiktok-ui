f5532e816608b133a01678d8d586d0133ae3cf5f
thanhhuy <huynt605@gmail.com>
2025-01-11 11:06:28 +0700

    Reset CSS Default CSS: font-family, font-size, line-height

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
-import React from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
-import HomePage from './pages/Home'
-import NewsPage from './pages/News'
-import ContactPage from './pages/Contact'
-
-import '~/App.css'
 
 function App() {
 
   return (
     <div className='app'>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/news'>News</Link>
-          </li>
-          <li>
-            <Link to='/contact'>Contact</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/news' element={<NewsPage />} />
-        <Route path='/contact' element={<ContactPage />} />
-      </Routes>
+     <h1>Nguyễn Thanh Huy</h1>
     </div>
   )
 }
